7e7942226cd0e937271f6ac18669bea6a7972831
keyuri <keyurirabadia@gmail.com>
2022-08-08 09:36:17 +0530

    practical1.2

--- MADPRACTICAL1_20012021045/src/main/kotlin/practical1.kt
+++ MADPRACTICAL1_20012021045/src/main/kotlin/practical1.kt
@@ -1,12 +1,12 @@
 fun main()
 {
-    var x : Int=22
+    var x : Int=45
     println("Integer Value : $x ")
-    var y = 1.5
+    var y = 45.5
     println("Float Value : $y ")
-    var z : Char= 'T'
+    var z : Char= 'K'
     println("Character Value : $z ")
-    var s : String="HMS"
+    var s : String="Keyoori"
     println("String Value : $s ")
     var b : Boolean= false
     println("Boolean value : $b ")
